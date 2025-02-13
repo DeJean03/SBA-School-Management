e73a4f1c11e76c83be888c70a49977ed7626ea22
DeJean03 <dejeanwilliams2003@gmail.com>
2025-02-12 23:19:44 -0800

    Add files via upload

--- src/main/java/sba/sms/models/Course.java
+++ src/main/java/sba/sms/models/Course.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -15,6 +16,82 @@ import java.util.Set;
  * of the relationship. Implement Lombok annotations to eliminate boilerplate code.
  */
 
+
+@Entity
+@Table(name = "course")
 public class Course {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "instructor", length = 50, nullable = false)
+    private String instructor;
+
+    @ManyToMany(mappedBy = "courses", fetch = FetchType.EAGER, cascade = {
+            CascadeType.DETACH, CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST
+    })
+    private Set<Student> students = new HashSet<>();
+
+    public Course() {
+    }
+
+    public Course(int id, String name, String instructor, Set<Student> students) {
+        this.id = id;
+        this.name = name;
+        this.instructor = instructor;
+        this.students = students;
+    }
+
+    public Course(String name, String instructor) {
+        this.name = name;
+        this.instructor = instructor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", instructor='" + instructor + '\'' +
+                ", students=" + students +
+                '}';
+    }
 }
+
