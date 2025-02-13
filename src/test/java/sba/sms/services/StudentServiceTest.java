c3c209a6e7411955bd3e4dd27850c56e5c674a2e
DeJean03 <dejeanwilliams2003@gmail.com>
2025-02-12 23:25:52 -0800

    Add files via upload

--- src/test/java/sba/sms/services/StudentServiceTest.java
+++ src/test/java/sba/sms/services/StudentServiceTest.java
@@ -3,18 +3,48 @@ package sba.sms.services;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sba.sms.models.Course;
 import sba.sms.models.Student;
 import sba.sms.utils.CommandLine;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 
-class StudentServiceTest {
+public class StudentServiceTest {
 
+    private StudentService studentService;
 
+    @BeforeEach
+    public void setUp() {
+        studentService = new StudentService();
+    }
+
+    @Test
+    public void testValidateStudent() {
+        studentService.setupStudentData();
+
+        boolean isValid = studentService.validateStudent("test@gmail.com", "password");
+        assertTrue(isValid, "The student should be valid with correct credentials");
+
+        boolean isInvalid = studentService.validateStudent("invalid@gmail.com", "wrongpassword");
+        assertFalse(isInvalid, "The student should be invalid with incorrect credentials");
+    }
+
+    @Test
+    public void testRegisterStudentToCourse() {
+        studentService.setupStudentData();
+
+        studentService.registerStudentToCourse("test@gmail.com", 1);
+
+        List<Course> courses = studentService.getStudentCourses("test@gmail.com");
+        assertEquals(1, courses.size(), "The student should be registered to one course");
+    }
 }
