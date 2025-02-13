12b998530a2c24d5151cbc8b9061de0ad9e61537
DeJean03 <dejeanwilliams2003@gmail.com>
2025-02-12 23:23:00 -0800

    Add files via upload

--- src/main/java/sba/sms/services/StudentService.java
+++ src/main/java/sba/sms/services/StudentService.java
@@ -1,10 +1,8 @@
 package sba.sms.services;
 
 import lombok.extern.java.Log;
-import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.query.NativeQuery;
 import org.hibernate.query.Query;
 import sba.sms.dao.StudentI;
 import sba.sms.models.Course;
@@ -13,6 +11,7 @@ import sba.sms.utils.HibernateUtil;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * StudentService is a concrete class. This class implements the
@@ -21,6 +20,95 @@ import java.util.List;
  * generate a logger file.
  */
 
-public class StudentService {
+@Log
+public class StudentService implements StudentI {
 
+    @Override
+    public void createStudent(Student student) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.save(student);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from Student", Student.class).list();
+        }
+    }
+
+    @Override
+    public Student getStudentByEmail(String email) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Student.class, email);
+        }
+    }
+
+    @Override
+    public boolean validateStudent(String email, String password) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Student> query = session.createQuery("from Student where email = :email and password = :password", Student.class);
+            query.setParameter("email", email);
+            query.setParameter("password", password);
+            return query.uniqueResult() != null;
+        }
+    }
+
+    @Override
+    public void registerStudentToCourse(String email, int courseId) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Student student = session.get(Student.class, email);
+            Course course = session.get(Course.class, courseId);
+            student.getCourses().add(course);
+            session.saveOrUpdate(student);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+
+    public List<Course> getStudentCourses(String email) {
+        Transaction transaction = null;
+        List<Course> courses = new ArrayList<>();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            Student student = session.get(Student.class, email);
+            if (student != null) {
+                Set<Course> courseSet = student.getCourses();
+                courses = new ArrayList<>(courseSet);
+            }
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return courses;
+
+
+    }
+
+    @Override
+    public void setupStudentData() {
+
+    }
 }
+
